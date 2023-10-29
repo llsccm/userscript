d48988d1fbd92d0a6fded638123d29b092590d3b
llsc <p8203@qq.com>
2023-10-30 02:46:10 +0800

    feat: singleton

--- SGS/SGSBase.user.js
+++ SGS/SGSBase.user.js
@@ -1,36 +1,51 @@
 // ==UserScript==
 // @name         SGS Base
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  三国杀依赖脚本
 // @author       ll
 // @match       http://web.sanguosha.com/*
 // @match       *://*.sanguosha.com/*
 // @match       *://game.iwan4399.com/yxsgs/*
 // @run-at      document-start
+// @license      MIT
 // @grant       none
 // ==/UserScript==
 
 ;(function () {
   'use strict'
-  window.WDVerSion = '1.0.0'
-  window.SGSMODULE = []
-  console._log = console.log
-  console._log('%cBASE', 'font-weight: bold; color: white; background-color: #525288; padding: 1px 4px; border-radius: 4px;')
-  const _log = function () {
-    const args = Array.prototype.slice.call(arguments)
-    console._log(...args)
-    Array.isArray(SGSMODULE) &&
-      SGSMODULE.forEach((fn) => {
-        fn(...arguments)
-      })
-  }
-  Object.defineProperty(console, 'log', {
-    get() {
-      return _log
-    },
-    set() {
-      return
+  window.SGSMODULE =
+    window.SGSMODULE ||
+    class MODULE {
+      constructor() {
+        window.WDVerSion = '1.0.0'
+        this._module = []
+        this.instance = null
+        console._log = console.log
+        console._log('%cBASE', 'font-weight: bold; color: white; background-color: #525288; padding: 1px 4px; border-radius: 4px;')
+        const _log = (...args) => {
+          console._log(...args)
+          this._module.forEach((fn) => {
+            fn(...args)
+          })
+        }
+        Object.defineProperty(console, 'log', {
+          get() {
+            return _log
+          },
+          set() {
+            return
+          }
+        })
+      }
+      static getInstance() {
+        if (!this.instance) {
+          this.instance = new MODULE()
+        }
+        return this.instance
+      }
+      addModule(fn) {
+        this._module.push(fn)
+      }
     }
-  })
 })()
